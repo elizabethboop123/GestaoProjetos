f86d07aa9336d0e1e9d82ba6b1f1279d7917fa75
paatrickreis <paatrick_reis@hotmail.com>
2015-08-24 16:21:12 -0300

    adicionando primeira versao

---
+++ Gestao_v1/gestao/gestaoapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from gestaoapp.models import *
+
+admin.site.register(Bolsista)
+admin.site.register(Coordenador)
+admin.site.register(Nucleo)
+admin.site.register(Projeto)
---
+++ Gestao_v1/gestao/gestaoapp/views/projeto.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+from gestaoapp.forms.projeto import FormProjeto
+from gestaoapp.models.projeto import Projeto
+
+class CadastroProjeto(View):
+
+	template = 'projeto/cadastro.html'
+
+	def get(self, request, projeto_id=None):
+
+		if projeto_id:
+			projeto = Projeto.objects.get(id=projeto_id)
+			form = FormProjeto(instance= projeto)
+			editar=True
+		else:
+			form = FormProjeto()
+			editar=False
+		
+		return render(request, self.template, {'form': form,'editar':editar})
+
+	def post(self, request, projeto_id=None):
+		
+		if projeto_id:
+			projeto = Projeto.objects.get(id=projeto_id)
+			form = FormProjeto(instance=projeto, data=request.POST)
+		else:
+
+			form = FormProjeto(request.POST)
+
+		if form.is_valid():
+			form.save(request)
+
+			return redirect('/')
+
+		else:
+			return render(request, self.template, {'form': form})
