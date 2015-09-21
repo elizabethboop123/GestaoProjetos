517d11a9cdd5554272906000bbc52ed439285873
Sabrina <sa.petrich@gmail.com>
2015-09-21 16:08:39 -0300

    vínculo, siape e matrícula

--- gestao/static/semantic/base.js
+++ gestao/static/semantic/base.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-	$('.dropdown').dropdown();
+	/*$('.dropdown').dropdown();*/
+	$('.vinculo').change(function(){
+		$('.vinculo option:selected').each(function(){
+
+			var selecionado = $(this).text();
+			if (selecionado.trim() == "Professor"){
+				$('.matricula').text('')
+				$('.matricula').text('SIAPE')
+				$('.periodo').hide()
+				$('.curso').hide()
+				$('.tres').removeClass('three').removeClass('fields')
+			}
+			else{
+				$('.matricula').text('')
+				$('.matricula').text('Matrícula')
+				$('.periodo').show()
+				$('.curso').show()
+				$('.tres').addClass('three').addClass('fields')
+			}
+		})
+		
+
+	})
 })
